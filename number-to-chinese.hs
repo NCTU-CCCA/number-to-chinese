77c51c4e4ae647c6fdfa8253db393bf66600c30f
Ting-Yen LAI <banacorn@gmail.com>
2013-06-15 16:37:16 +0800

    duh

---
+++ number-to-chinese.hs
@@ -0,0 +1,13 @@
+convert '1' = '一'
+convert '2' = '二'
+convert '3' = '三'
+convert '4' = '四'
+convert '5' = '五'
+convert '6' = '六'
+convert '7' = '七'
+convert '8' = '八'
+convert '9' = '九'
+convert '0' = '十'
+convert '.' = '點'
+
+numberToChinese = map convert
