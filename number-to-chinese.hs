2f27e7938781effc3692279dfd129367312e61b9
Ting-Yen LAI <banacorn@gmail.com>
2013-06-15 16:50:51 +0800

    typo, fuck this

--- number-to-chinese.hs
+++ number-to-chinese.hs
@@ -13,11 +13,11 @@ convert '.' = '點'
 
 numberToChinese = map convert
 
--- dictionay version
-dictionay = [
+-- dictionary version
+dictionary = [
     ('1', '一'), ('2', '二'), ('3', '三'), ('4', '四'), ('5', '五'), 
     ('6', '六'), ('7', '七'), ('8', '八'), ('9', '九'), ('0', '零'), 
     ('.', '點')
     ]
 
-numberToChinese' = mapM (flip lookup dictionay)
+numberToChinese' = mapM (flip lookup dictionary)
