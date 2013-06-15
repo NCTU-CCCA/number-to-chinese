f9b0be45a98c83edcc14a8f1f65dd1b2396dc610
Ting-Yen LAI <banacorn@gmail.com>
2013-06-15 16:49:56 +0800

    haskell dict lookup version

--- number-to-chinese.hs
+++ number-to-chinese.hs
@@ -1,3 +1,4 @@
+-- vanilla version
 convert '1' = '一'
 convert '2' = '二'
 convert '3' = '三'
@@ -11,3 +12,12 @@ convert '0' = '零'
 convert '.' = '點'
 
 numberToChinese = map convert
+
+-- dictionay version
+dictionay = [
+    ('1', '一'), ('2', '二'), ('3', '三'), ('4', '四'), ('5', '五'), 
+    ('6', '六'), ('7', '七'), ('8', '八'), ('9', '九'), ('0', '零'), 
+    ('.', '點')
+    ]
+
+numberToChinese' = mapM (flip lookup dictionay)
