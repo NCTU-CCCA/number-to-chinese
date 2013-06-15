e8e3933764fe43bc06969f7a0e88a138dfee18e4
Ting-Yen LAI <banacorn@gmail.com>
2013-06-15 17:00:41 +0800

    nota bene

--- number-to-chinese.hs
+++ number-to-chinese.hs
@@ -13,7 +13,7 @@ convert '.' = '點'
 
 numberToChinese = map convert
 
--- dictionary version
+-- monadic dictionary lookup fail-safe version
 dictionary = [
     ('1', '一'), ('2', '二'), ('3', '三'), ('4', '四'), ('5', '五'), 
     ('6', '六'), ('7', '七'), ('8', '八'), ('9', '九'), ('0', '零'), 
